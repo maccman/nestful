32942abca20685b3546362ffeadf8e50b3d0705c
Alex MacCaw <alex@alexmaccaw.com>
2015-07-08 17:06:22 -0700

    whitespace

--- lib/nestful/connection.rb
+++ lib/nestful/connection.rb
@@ -84,34 +84,34 @@ module Nestful
     # Handles response and error codes from the remote service.
     def handle_response(response)
       case response.code.to_i
-        when 200...299
-          response
-        when 300..399
-          raise Redirection.new(response)
-        when 400
-          raise BadRequest.new(response)
-        when 401
-          raise UnauthorizedAccess.new(response)
-        when 403
-          raise ForbiddenAccess.new(response)
-        when 404
-          raise ResourceNotFound.new(response)
-        when 405
-          raise MethodNotAllowed.new(response)
-        when 409
-          raise ResourceConflict.new(response)
-        when 410
-          raise ResourceGone.new(response)
-        when 422
-          raise ResourceInvalid.new(response)
-        when 401...500
-          raise ClientError.new(response)
-        when 500...600
-          raise ServerError.new(response)
-        else
-          raise ResponseError.new(
-            response, "Unknown response code: #{response.code}"
-          )
+      when 200...299
+        response
+      when 300..399
+        raise Redirection.new(response)
+      when 400
+        raise BadRequest.new(response)
+      when 401
+        raise UnauthorizedAccess.new(response)
+      when 403
+        raise ForbiddenAccess.new(response)
+      when 404
+        raise ResourceNotFound.new(response)
+      when 405
+        raise MethodNotAllowed.new(response)
+      when 409
+        raise ResourceConflict.new(response)
+      when 410
+        raise ResourceGone.new(response)
+      when 422
+        raise ResourceInvalid.new(response)
+      when 401...500
+        raise ClientError.new(response)
+      when 500...600
+        raise ServerError.new(response)
+      else
+        raise ResponseError.new(
+          response, "Unknown response code: #{response.code}"
+        )
       end
     end
 
